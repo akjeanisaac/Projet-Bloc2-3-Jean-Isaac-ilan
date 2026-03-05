92752aa1d7a0b143c7975d01247843af32616e2a
akjeanisaac <akjeanisaac.pro@gmail.com>
2026-03-05 15:27:54 +0100

    05/03/2026 Projet Menu

---
+++ ProjetMenuGroup/ProjetMenuGroup/Edition.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetMenuGroup
+{
+    /// <summary>
+    /// Logique d'interaction pour Edition.xaml
+    /// </summary>
+    public partial class Edition : Window
+    {
+        public Edition()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnComp_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnElem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
